77ad444f7c2baa407b8186080cf893b9266449b6
Alex | Kronox <alexander_vogt@outlook.de>
2025-01-20 10:22:56 +0100

    github deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig((userConfig: UserConfig) => {
+  let base = '/'
+  switch (userConfig.mode) {
+    case 'deploy':
+      base = '/ba-prototype/'
+      break
+  }
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+    base: base
+  }
 })
