62c125e8461ed8992b5e84a7a96733eaa860c47f
Alex | Kronox <alexander_vogt@outlook.de>
2025-01-20 10:12:32 +0100

    true

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Temporary</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/stores/index.ts
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+
+export const store = defineStore({
+  id: 'main',
+  state: () => ({
+    // state
+  }),
+  actions: {
+    // actions
+  },
+  getters: {
+    // getters
+  }
+})
