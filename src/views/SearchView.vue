b2584c4842a9b1bfece1b42d80067fb28de30905
Alex | Kronox <alexander_vogt@outlook.de>
2025-01-27 13:04:10 +0100

    related inserts section

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="flex w-full justify-center py-5">
     <div></div>
-    <div class="w-[800px] space-y-5">
-      <InsertBanner v-for="insert of store().inserts" :key="insert.id" :insert="insert" />
+    <div class="w-[800px]">
+      <div class="space-y-5">
+        <InsertBanner v-for="insert of store().inserts" :key="insert.id" :insert="insert" />
+      </div>
+
+      <hr class="mb-2 mt-5 border-t border-gray-300" />
+
+      <h2 class="mb-2">Related inserts:</h2>
+      <div class="space-y-5">
+        <InsertBanner v-for="insert of store().inserts" :key="insert.id" :insert="insert" />
+      </div>
     </div>
   </div>
 </template>
